bf1f0b022c1d42bfab142ca65dea56a8b1256456
Hodayousry2752 <hanaayoussrey@gmail.com>
2026-01-26 18:36:52 +0200

    updates

--- portfolio/src/hooks/useLenis.ts
+++ portfolio/src/hooks/useLenis.ts
@@ -1,14 +1,23 @@
-import { useEffect } from "react";
 import Lenis from "@studio-freight/lenis";
+import { useEffect } from "react";
 
+export function useLenis() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      lerp: 0.08,
+      wheelMultiplier: 1,
+      touchMultiplier: 1,
+    });
 
-export const useLenis = () => {
-useEffect(() => {
-const lenis = new Lenis({ smooth: true, lerp: 0.08 });
-const raf = (time: number) => {
-lenis.raf(time);
-requestAnimationFrame(raf);
-};
-requestAnimationFrame(raf);
-}, []);
-};
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+}
