ceb4f575b8d3850347d7f94ec28d39ec91e884b1
Hodayousry2752 <hanaayoussrey@gmail.com>
2026-01-27 18:30:10 +0200

    updated router

--- portfolio/src/app/router.tsx
+++ portfolio/src/app/router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import Layout from "../components/layout/Layout";
 import Home from "../pages/Home";
 import About from "../pages/About";
@@ -7,18 +7,16 @@ import Projects from "../pages/Projects";
 import Experience from "../pages/Experience";
 import Contact from "../pages/Contact";
 
-
-export const router = createBrowserRouter([
-{
-element: <Layout />,
-children: [
-{ path: "/", element: <Home /> },
-{ path: "/about", element: <About /> },
-{ path: "/skills", element: <Skills /> },
-{ path: "/projects", element: <Projects /> },
-{ path: "/experience", element: <Experience /> },
-{ path: "/contact", element: <Contact /> },
-],
-},
+export const router = createHashRouter([
+  {
+    element: <Layout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/about", element: <About /> },
+      { path: "/skills", element: <Skills /> },
+      { path: "/projects", element: <Projects /> },
+      { path: "/experience", element: <Experience /> },
+      { path: "/contact", element: <Contact /> },
+    ],
+  },
 ]);
-
